35cc2a1464bd162c06f97f9c4efa76b05626da96
Tommy H. Yeargin, Jr <yeargin2022@gmail.com>
2022-11-29 13:52:05 -0500

    Initial Commit

---
+++ CountToTen/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  CountToTen
+//
+//  Created by hacking on 11/29/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
